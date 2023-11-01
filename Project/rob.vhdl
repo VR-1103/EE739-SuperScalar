f2fd17a78d9822f67dfb37d0a0a475de36cbf113
aryavishe@kubuntu <arya@kubuntu>
2023-11-01 19:35:46 +0530

    Rob empty file added

---
+++ Project/rob.vhdl
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+use work.Gates.all;
+
+entity rob is
+    port(dispatch_word1, dispatch_word2: in std_logic_vector(len_PC +  downto 0); --I am assuming that each PC is
+    );
+end entity;
